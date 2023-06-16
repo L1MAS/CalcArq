b41b404fe764547afbb615c3386280848f19267f
L1MAS <luana3.limas@gmail.com>
2023-06-16 17:56:25 -0300

    Nenhuma mudança

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function pegarElemento(name)
 {
     return document.getElementsByName(name)[0]
@@ -24,13 +23,14 @@ function irParaMenuPrincipal()
 function calcularArea()
 {
     const input1 = pegarElemento("i1")
-    const input2 = pegarElemento("i2")
+    const input2 = pegarElemento("i2")    
     const areaTerreno = input2.value
     const areaPermeavel = input1.value
 
     const taxaPermeabilidade = areaPermeavel * 100 / areaTerreno
 
-    pegarElemento("resultado").value=taxaPermeabilidade
+    const inputResultado = pegarElemento("resultado")
+    inputResultado.value=taxaPermeabilidade
 
     pegarElemento("resultado").style.display = "inline";
     pegarElemento("textoResultado").style.display = "inline";
